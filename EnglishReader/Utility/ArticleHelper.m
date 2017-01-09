b9a45c0ad22488e16f5d6694171b7181c69f7950
QMTV <1531724247@qq.com>
2017-01-09 14:14:51 +0800

    添加对PDF格式的支持

--- EnglishReader/Utility/ArticleHelper.m
+++ EnglishReader/Utility/ArticleHelper.m
@@ -10,6 +10,7 @@
 #import <ctype.h>
 #import "YYKit.h"
 #import "UIWebView+JS.h"
+#import <TET_ios/TET_objc.h>
 
 @interface ArticleHelper ()<UIWebViewDelegate>
 
@@ -94,7 +95,6 @@
     if ([self.delegate respondsToSelector:@selector(articleHelper:handleSuccessedWithActionText:)]) {
         [self.delegate articleHelper:self handleSuccessedWithActionText:actionText];
     }
-    
     /*NSAttributedString--rtf--to
      NSAttributedString *str = [[NSAttributedString alloc] initWithString:@"YOLO" attributes:nil];
      NSData *data = [str dataFromRange:(NSRange){0, [str length]} documentAttributes:@{NSDocumentTypeDocumentAttribute: NSRTFTextDocumentType} error:NULL];
@@ -108,14 +108,16 @@
     [self.webView loadHTMLString:htmlString baseURL:nil];
     
     //UILabel显示HTML文本
-    NSAttributedString * attrStr = [[NSAttributedString alloc] initWithData:[htmlString dataUsingEncoding:NSUnicodeStringEncoding] options:@{NSDocumentTypeDocumentAttribute:NSHTMLTextDocumentType} documentAttributes:nil error:nil];
+    NSAttributedString *attrStr = [[NSAttributedString alloc] initWithData:[htmlString dataUsingEncoding:NSUnicodeStringEncoding] options:@{NSDocumentTypeDocumentAttribute:NSHTMLTextDocumentType} documentAttributes:nil error:nil];
 }
 
 //处理pdf文本
 - (void)handlePDFWithFilePath:(NSString *)filePath {
-    NSURL *url = [NSURL URLWithString:filePath];
-    NSURLRequest *urlRequest = [[NSURLRequest alloc] initWithURL:url];
-    [self.webView loadRequest:urlRequest];
+    NSString *pdfContent = [self extractTextFromPDFWithFilePath:filePath];
+    NSAttributedString *actionText = [self actionTextWithText:pdfContent];
+    if ([self.delegate respondsToSelector:@selector(articleHelper:handleSuccessedWithActionText:)]) {
+        [self.delegate articleHelper:self handleSuccessedWithActionText:actionText];
+    }
 }
 
 - (NSArray *)analyseArticleText:(NSString *)articleText {
@@ -217,6 +219,139 @@
     [actionText setLineSpacing:8.0];//设置行间距
     
     return actionText;
+}
+
+- (NSString *)extractTextFromPDFWithFilePath:(NSString *)filePath {
+    
+    // Find our documents directory
+    NSArray *dirPaths;
+    NSString *documentsDir;
+    dirPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    documentsDir = [dirPaths objectAtIndex:0];
+    
+    /* for simplicity we use hardcoded filenames */
+    NSString *infile= filePath;
+    
+    /* global option list */
+    NSString *globaloptlist =
+    [NSString stringWithFormat:@"searchpath={{%@} {%@/EnglishReader.app} {%@/EnglishReader.app/resource/cmap}}",
+     documentsDir, NSHomeDirectory(), NSHomeDirectory()];
+    
+    /* option list to switch on TET logging */
+    //NSString *loggingoptlist = [NSString stringWithFormat:@"logging {filename={%@/trace.txt} remove}", documentsDir];
+    
+    /* document-specific option list */
+    NSString *docoptlist = @"";
+    
+    /* page-specific option list */
+    NSString *pageoptlist = @"granularity=page";
+    
+    /* separator to emit after each chunk of text. This depends on the
+     * application's needs;
+     * for granularity=word a space character may be useful.
+     */
+#define SEPARATOR @"\n"
+    
+    int pageno = 0;
+    NSMutableString *pdfText = [[NSMutableString alloc]init];;
+    NSString *warningText=@"";
+    
+    TET *tet = [[TET alloc] init];
+    if (!tet) {
+        return pdfText;
+    }
+    
+    @try {
+        NSInteger n_pages;
+        NSInteger doc;
+        
+        //[tet set_option:loggingoptlist];
+        [tet set_option:globaloptlist];
+        
+        doc = [tet open_document:infile optlist:docoptlist];
+        
+        if (doc == -1)
+        {
+            warningText = [NSString stringWithFormat:@"Error %ld in %@(): %@\n",
+                           (long)[tet get_errnum], [tet get_apiname], [tet get_errmsg]];
+            [self displayError:warningText];
+            return pdfText;
+        }
+        
+        /* get number of pages in the document */
+        n_pages = (NSInteger) [tet pcos_get_number:doc path:@"length:pages"];
+        
+        /* loop over pages in the document */
+        for (pageno = 1; pageno <= n_pages; ++pageno)
+        {
+            NSString *text;
+            NSInteger page;
+            
+            page = [tet open_page:doc pagenumber:pageno optlist:pageoptlist];
+            
+            if (page == -1)
+            {
+                warningText = [NSString stringWithFormat:@"%@\nError %ld in %@() on page %d: %@\n",
+                               warningText, (long)[tet get_errnum], [tet get_apiname], pageno, [tet get_errmsg]];
+                continue;                        /* try next page */
+            }
+            
+            /* Retrieve all text fragments; This is actually not required
+             * for granularity=page, but must be used for other granularities.
+             */
+            while ((text = [tet get_text:page]) != nil)
+            {
+                [pdfText appendString:text];
+                [pdfText appendString:SEPARATOR];
+            }
+            
+            if ([tet get_errnum] != 0)
+            {
+                warningText  = [NSString stringWithFormat:@"%@\nError %ld in %@() on page %d: %@\n", warningText, (long)[tet get_errnum], [tet get_apiname], pageno, [tet get_errmsg]];
+            }
+            
+            [tet close_page:page];
+        }
+        
+        [tet close_document:doc];
+    }
+    
+    @catch (TETException *ex) {
+        NSString *exception=@"";
+        if (pageno == 1) {
+            exception = [NSString stringWithFormat:@"Error %ld in %@(): %@\n",
+                         (long)[ex get_errnum], [ex get_apiname], [ex get_errmsg]];
+        } else {
+            exception = [NSString stringWithFormat:@"Error %ld in %@() on page %d: %@\n",
+                         (long)[ex get_errnum], [ex get_apiname], pageno, [ex get_errmsg]];
+        }
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"TET crashed" message:exception
+                                                           delegate:self cancelButtonTitle:nil otherButtonTitles:@"ok", nil];
+        [alertView show];
+    }
+    @catch (NSException *ex) {
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:[ex name] message:[ex reason]
+                                                           delegate:self cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
+        [alertView show];
+    }
+    @finally {
+        if (tet)
+            tet = nil;
+    }
+    
+    
+    /* show the warning(s) that occured while processing the file */
+    if (warningText.length>0) {
+        [self displayError:warningText];
+    }
+    
+    return pdfText;
+}
+
+
+- (void) displayError: (NSString *) message {
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"TET error" message:message delegate:nil cancelButtonTitle:nil otherButtonTitles:@"ok", nil];
+    [alertView show];
 }
 
 #pragma mark ----- UIWebViewDelegate
