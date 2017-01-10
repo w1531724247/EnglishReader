841608a75cd5192815f0ef54fa08b76b539990ce
QMTV <1531724247@qq.com>
2017-01-10 19:02:27 +0800

    调整文件结构

---
+++ EnglishReader/APPFunction/WordList/Models/WordModel.m
@@ -0,0 +1,13 @@
+//
+//  WordModel.m
+//  EnglishReader
+//
+//  Created by QMTV on 17/1/10.
+//  Copyright © 2017年 LFC. All rights reserved.
+//
+
+#import "WordModel.h"
+
+@implementation WordModel
+
+@end
