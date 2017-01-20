f296bd1975c52bb5658ad13b888ebe4e2e655161
LFC <1531724247@qq.com>
2017-01-20 17:03:01 +0800

    添加单词本的section分段

--- APPModules/WordList/DataSource/WordListDataSource.m
+++ APPModules/WordList/DataSource/WordListDataSource.m
@@ -14,6 +14,7 @@
 #import "StrangeWordTable.h"
 #import "NSDictionary+JSONHelper.h"
 #import "WordRecordModel.h"
+#import "WordListSectionHeaderView.h"
 
 @interface WordListDataSource ()
 
@@ -95,6 +96,19 @@
 
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
     return YES;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    NSString *identifier = @"WordListSectionHeaderView";
+    WordListSectionHeaderView *sectionHeaderView = [[WordListSectionHeaderView alloc] initWithReuseIdentifier:identifier];
+    NSString *sectionTitle = [self.sectionTitleArray objectAtIndex:section];
+    sectionHeaderView.sectionTitle = sectionTitle;
+    
+    return sectionHeaderView;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return 20.0;
 }
 
 #pragma mark ---- UITableViewDelegate
---
+++ APPModules/WordList/Views/WordListSectionHeaderView.h
@@ -0,0 +1,15 @@
+//
+//  WOrdListSectionHeaderView.h
+//  EnglishReader
+//
+//  Created by LFC on 17/1/20.
+//  Copyright © 2017年 LFC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WordListSectionHeaderView : UITableViewHeaderFooterView
+
+@property (copy, nonatomic) NSString *sectionTitle;
+
+@end
